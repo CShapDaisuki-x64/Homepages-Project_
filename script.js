c5ad791e4999cfff00010e3b2d6199c4b9bcc8fb
CShapDaisuki-x64 <reokos2014sub@gmail.com>
2026-06-07 15:42:43 +0900

    S3.2.0_D2.2.4_R1.0.0

--- script.js
+++ script.js
@@ -13,3 +13,6 @@ fetch(url)
   fetch("header.html")
   .then((response) => response.text())
   .then((data) => document.querySelector("#hedaer_div").innerHTML = data);
+  fetch("footer.html")
+  .then((response) => response.text())
+  .then((data) => document.querySelector("#footer_div").innerHTML = data);
