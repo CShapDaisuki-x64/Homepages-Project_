5ffaae650d71da27053e0636d625bc48887afbe4
﻿CShapDaisuki-x64 <235272469+CShapDaisuki-x64@users.noreply.github.com>
2026-07-14 20:33:46 +0900

    S3.5.8_D2.3.2_R1.0.6_V1.0.1

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-const url = "https://api.rss2json.com/v1/api.json?rss_url=https://syumiprogrammer256.web.fc2.com/rss.xml";
-fetch(url)
-  .then(res => res.json())
-  .then(data => {
-    const list = document.getElementById("rss");
-    list.innerHTML="";
-    data.items.forEach(item => {
-      const li = document.createElement("li");
-      li.innerHTML = `<a href="${item.link}">${item.title}</a>`;
-      list.appendChild(li);
-    });
-  });
   fetch("header.html")
   .then((response) => response.text())
   .then((data) => document.querySelector("#hedaer_div").innerHTML = data);
