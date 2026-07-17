03a433dea9d6daf860800168a56a87c71c126fd7
CShapDaisuki-x64 <reokos2014sub@gmail.com>
2026-07-18 01:13:47 +0900

    S3.6.0_D2.3.3_R1.1.0_V1.0.1_1 \tへの対応やコードを見やすくする等

--- Video.js
+++ Video.js
@@ -2,59 +2,59 @@ const ShopBox = document.querySelector('.Videos');
 let saved_Items;
 async function LoadJson()
 {
-    let response = await fetch('video.json');
-    saved_Items = await response.json();
-    console.log(saved_Items["1s"]["title"])
-    Load_View(saved_Items);
-    scrollToHash();
+	let response = await fetch('video.json');
+	saved_Items = await response.json();
+	console.log(saved_Items["1s"]["title"])
+	Load_View(saved_Items);
+	scrollToHash();
 }
 function Load_View(items)
 {
-    ShopBox.innerHTML = '';
-    Object.keys(items).forEach(key => {
-        const item = items[key];
-        const btn = document.createElement('div');
-        btn.className = "videosty";
-        btn.id = key;
-        if(item.topic.nicovideonone)
-        {
-            btn.innerHTML = "<div class='title'>"+item.title+"</div>ID:"+key+"<br>ニコニコ動画:動画なし<br>YouTube:<a href='https://youtu.be/"+item.youtube+"'>https://youtu.be/"+item.youtube+"</a>";
-        }
-        else
-        {
-            btn.innerHTML = "<div class='title'>"+item.title+"</div>ID:"+key+"<br>ニコニコ動画:<a href='https://www.nicovideo.jp/watch/"+item.nicovideo+"'>https://www.nicovideo.jp/watch/"+item.nicovideo+"</a><br>YouTube:<a href='https://youtu.be/"+item.youtube+"'>https://youtu.be/"+item.youtube+"</a>";
-        }
-        if(item.topic.VoiceBig)
-        {
-            btn.innerHTML = btn.innerHTML + "<div class='EX'>⚠注意:騒音</div>"
-        }
-        if(item.topic.remake)
-        {
-            btn.innerHTML = btn.innerHTML + "<div class='info'>🛈️情報:リメイク<br>元動画ID:<a href='video.html#"+item.topic.remake2+"'>"+item.topic.remake2+"</a></div>"
-        }
-        ShopBox.appendChild(btn);
+	ShopBox.innerHTML = '';
+	Object.keys(items).forEach(key => {
+		const item = items[key];
+		const btn = document.createElement('div');
+		btn.className = "videosty";
+		btn.id = key;
+		if(item.topic.nicovideonone)
+		{
+			btn.innerHTML = "<div class='title'>"+item.title+"</div>ID:"+key+"<br>ニコニコ動画:動画なし<br>YouTube:<a href='https://youtu.be/"+item.youtube+"'>https://youtu.be/"+item.youtube+"</a>";
+		}
+		else
+		{
+			btn.innerHTML = "<div class='title'>"+item.title+"</div>ID:"+key+"<br>ニコニコ動画:<a href='https://www.nicovideo.jp/watch/"+item.nicovideo+"'>https://www.nicovideo.jp/watch/"+item.nicovideo+"</a><br>YouTube:<a href='https://youtu.be/"+item.youtube+"'>https://youtu.be/"+item.youtube+"</a>";
+		}
+		if(item.topic.VoiceBig)
+		{
+			btn.innerHTML = btn.innerHTML + "<div class='EX'>⚠注意:騒音</div>"
+		}
+		if(item.topic.remake)
+		{
+			btn.innerHTML = btn.innerHTML + "<div class='info'>🛈️情報:リメイク<br>元動画ID:<a href='video.html#"+item.topic.remake2+"'>"+item.topic.remake2+"</a></div>"
+		}
+		ShopBox.appendChild(btn);
 
-    });
+	});
 }
 function scrollToHash() {
-    const hash = window.location.hash;
-    if (hash) {
-        const targetElement = document.getElementById(hash.substring(1));
-        if (targetElement) {
-            targetElement.scrollIntoView({ behavior: 'auto', block: 'start' });
-        }
-    }
+	const hash = window.location.hash;
+	if (hash) {
+		const targetElement = document.getElementById(hash.substring(1));
+		if (targetElement) {
+			targetElement.scrollIntoView({ behavior: 'auto', block: 'start' });
+		}
+	}
 }
 const text_form = document.getElementById("ID");
 
 text_form.addEventListener("keydown", test_event);
 
 function test_event(e) {
-  if (e.key === "Enter") {
-    e.preventDefault();
-    window.location.href = 'video.html#' + document.getElementById('ID').value;
-  }  
-  return false;
+	if (e.key === "Enter") {
+	e.preventDefault();
+	window.location.href = 'video.html#' + document.getElementById('ID').value;
+	}	
+	return false;
 }
 window.addEventListener('hashchange', scrollToHash);
 LoadJson();
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-  fetch("header.html")
-  .then((response) => response.text())
-  .then((data) => document.querySelector("#hedaer_div").innerHTML = data);
-  fetch("footer.html")
-  .then((response) => response.text())
-  .then((data) => document.querySelector("#footer_div").innerHTML = data);
+	fetch("header.html")
+	.then((response) => response.text())
+	.then((data) => document.querySelector("#hedaer_div").innerHTML = data);
+	fetch("footer.html")
+	.then((response) => response.text())
+	.then((data) => document.querySelector("#footer_div").innerHTML = data);
