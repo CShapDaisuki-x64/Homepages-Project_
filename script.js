3919ef516ed38c9fbd8b13904c10938c2a253c76
CShapDaisuki-x64 <reokos2014sub@gmail.com>
2026-08-03 18:12:43 +0900

    S3.7.1_D2.3.8_M1.0.5_G1.0.1_U1.0.1_R1.1.5_V1.0.5_1 デザイン設定等

--- script.js
+++ script.js
@@ -13,3 +13,58 @@ if(localStorage.getItem('Site_OK') != "true")
 	document.body.appendChild(bur);
 	bur.show();
 }
+let style_html = document.createElement('style');
+if(localStorage.getItem("site_dark")=="true")
+{
+	style_html.innerHTML= `	:root{
+		--bck_color:#444;
+		--haf_bck_color:#16b;
+		--haf_link_in_color:#bbb;
+		--haf_img_rod:invert(100%);
+		--acs_color:#ddd;
+		--btn_bck_color:#bbb;
+		--btn_bdr_color:#999;
+		--btn_in_img_rod:brightness(120%);
+		--link_color:#29d;
+		--link_color_ace:#b5e;
+		--link_color_ace_in:#d7e;
+		--kmk-vdo_bck_color:#27b;
+		--vdo_acs_color:#ddd;
+		--vdo_link_color:#cdd;
+		--vdo_link_in_color:#bbb;
+	} `;
+}
+else if(localStorage.getItem("site_dark")=="false")
+{
+	style_html.innerHTML= `:root{
+	--bck_color:#eee;
+	--haf_bck_color:#0ce;
+	--haf_link_in_color:#456;
+	--haf_img_rod:invert(0%);
+	--acs_color:#112;
+	--btn_bck_color:#ddd;
+	--btn_bdr_color:#bbb;
+	--btn_bck_in_color:#ccc;
+	--btn_bdr_in_color:#aaa;
+	--btn_in_img_rod:brightness(80%);
+	--link_color:#07d;
+	--link_color_ace:#90e;
+	--link_color_in:#1cd;
+	--link_color_ace_in:#c1d;
+	--kmk-vdo_bck_color:#0ad;
+	--vdo_acs_color:#eee;
+	--vdo_link_color:#cde;
+	--vdo_link_in_color:#dee;
+	--vdo_ex_bck_color:#e05;
+	--vdo_inf_bck_color:#05e;
+}`;
+
+}
+else{
+
+}
+if(localStorage.getItem("site_color"))
+{
+	style_html.innerHTML = style_html.innerHTML+":root{--haf_bck_color:"+localStorage.getItem('site_color')+";}"
+}
+document.body.appendChild(style_html);
