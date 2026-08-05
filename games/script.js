c2d5af877b64c14f357d2c736ef797e807e71d2f
﻿CShapDaisuki-x64 <235272469+CShapDaisuki-x64@users.noreply.github.com>
2026-08-05 20:33:56 +0900

    S3.7.2_D2.3.9_M1.0.6_G1.0.2_U1.0.2_R1.1.5_V1.0.5_1 スタイル管理をJS化 外部リンクアイコンを追加

--- games/script.js
+++ games/script.js
@@ -11,6 +11,72 @@ if(localStorage.getItem('Site_OK') != "true")
 	document.body.appendChild(bur);
 	bur.show();
 }
+let style_html = document.createElement('style');
+const thema = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const dark =
+`	:root{
+		--bck_color:#444;
+		--haf_bck_color:#16b;
+		--haf_link_in_color:#bbb;
+		--haf_img_rod:invert(100%);
+		--acs_color:#ddd;
+		--btn_bck_color:#bbb;
+		--btn_bdr_color:#999;
+		--btn_in_img_rod:brightness(120%);
+		--link_color:#29d;
+		--link_color_ace:#c6e;
+		--link_color_ace_in:#e9e;
+		--kmk-vdo_bck_color:#27b;
+		--vdo_acs_color:#ddd;
+		--vdo_link_color:#cdd;
+		--vdo_link_in_color:#bbb;
+	} `;
+const notdark =
+`:root{
+	--bck_color:#eee;
+	--haf_bck_color:#0ce;
+	--haf_link_in_color:#456;
+	--haf_img_rod:invert(0%);
+	--acs_color:#112;
+	--btn_bck_color:#ddd;
+	--btn_bdr_color:#bbb;
+	--btn_bck_in_color:#ccc;
+	--btn_bdr_in_color:#aaa;
+	--btn_in_img_rod:brightness(80%);
+	--link_color:#07d;
+	--link_color_ace:#90e;
+	--link_color_in:#1cd;
+	--link_color_ace_in:#c1d;
+	--kmk-vdo_bck_color:#0ad;
+	--vdo_acs_color:#eee;
+	--vdo_link_color:#cde;
+	--vdo_link_in_color:#dee;
+	--vdo_ex_bck_color:#e05;
+	--vdo_inf_bck_color:#05e;
+}`;
+if(localStorage.getItem("site_dark")=="true")
+{
+	style_html.innerHTML=dark;
+}
+else if(localStorage.getItem("site_dark")=="false")
+{
+	style_html.innerHTML= notdark;
+}
+else{
+	if(thema)
+	{
+		style_html.innerHTML=dark;
+	}
+	else
+	{
+		style_html.innerHTML=notdark;
+	}
+}
+if(localStorage.getItem("site_color"))
+{
+	style_html.innerHTML = style_html.innerHTML+":root{--haf_bck_color:"+localStorage.getItem('site_color')+";}"
+}
+document.body.appendChild(style_html);
 let points = 0;
 let points_local=localStorage.getItem("games_points")
 if (points_local !== null)
--- very_long_url_ja_jp_hello_hi_kawaii_very_long_url/script.js
+++ very_long_url_ja_jp_hello_hi_kawaii_very_long_url/script.js
@@ -1,9 +1,3 @@
-fetch("/header.html")
-	.then((response) => response.text())
-	.then((data) => document.querySelector("#hedaer_div").innerHTML = data);
-fetch("/footer.html")
-	.then((response) => response.text())
-	.then((data) => document.querySelector("#footer_div").innerHTML = data);
 if(localStorage.getItem('Site_OK') != "true")
 {
 	let bur = document.createElement('dialog');
