129862b36425f74e2a11cd568d67c16d3e9dc015
﻿CShapDaisuki-x64 <235272469+CShapDaisuki-x64@users.noreply.github.com>
2026-08-07 08:22:54 +0900

    S3.7.3_D2.3.9_M1.0.7_G1.0.3_U1.0.3_R1.1.5_V1.0.5_1 jsのユニット化 プロフィールを分離

--- mode_key/script.js
+++ mode_key/script.js
@@ -1,13 +1,6 @@
-if(localStorage.getItem('Site_OK') != "true")
-{
-	let bur = document.createElement('dialog');
-	bur.id="bur";
-	bur.style="position:fixed;bottom:0px;left:0px;right:0px;margin:0px;width: 100%;";
-	bur.innerHTML=`<iframe width="200" height="300px" src='/license.js.html'><a href='/license.html'>iframe対応してない人はここ</a></iframe><button onclick='localStorage.setItem("Site_OK","true");this.closest("dialog").close();this.closest("dialog").remove();'>同意する</button><button onclick='localStorage.setItem("Site_OK","false");window.location.href="/license.go.html";'>同意しない</button>`;
-	document.body.appendChild(bur);
-	bur.show();
-}
-function open_window(open_window_name, open_window_title) {
+import * as allfile from '/allfile.js';
+allfile.open_main();
+export function open_window(open_window_name, open_window_title) {
 	console.log('open_window 開始:', open_window_name);
 
 	// 1. テンプレートから要素を探して複製
@@ -119,6 +112,7 @@ function open_window(open_window_name, open_window_title) {
 }
 function maxto()
 {
+	console.log("ok")
 	if(document.fullscreenElement)
 	{
 		document.exitFullscreen();
@@ -129,3 +123,4 @@ function maxto()
 		document.body.requestFullscreen();
 	}
 }
+window.maxto=maxto;
