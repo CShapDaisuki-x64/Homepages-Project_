a5c81c32304d07556476803c3a4bfc7129520ebb
CShapDaisuki-x64 <235272469+CShapDaisuki-x64@users.noreply.github.com>
2026-09-09 19:29:09 +0900

    S4.0.5_D2.5.3_A1.0.0_M1.2.3_G1.2.4_U1.2.3_R2.0.1_V1.0.8_1 (#4)

--- go.ps1
+++ go.ps1
@@ -1,6 +1,6 @@
 Get-ChildItem -Recurse -Filter *.js |
 Where-Object { $_.Name -notlike '*.min.js' } |
 ForEach-Object {
-    $out = $_.FullName -replace '\.js$', '.min.js'
-    npx esbuild $_.FullName --minify-syntax --minify-whitespace --outfile=$out
+		$out = $_.FullName -replace '\.js$', '.min.js'
+		npx esbuild $_.FullName --minify-syntax --minify-whitespace --outfile=$out
 }
