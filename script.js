9aa85d090d49b5d7a9554f67630490d0ec443749
CShapDaisuki-x64 <235272469+CShapDaisuki-x64@users.noreply.github.com>
2026-09-11 23:12:06 +0900

    Revert "S4.2.0_D2.5.3_A1.0.0_M1.2.3_G1.2.4_U1.2.3_R2.0.2_V1.0.8_1"
    This reverts commit 4d57091d1e20db2ac60075c4164d6f2978f7b246.

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ import * as allfile from './allfile.js';
 allfile.open_main();
 allfile.footer("/header.html","hedaer_div");
 allfile.footer("/footer.html","footer_div");
-allfile.footer("/menu.html","menu");
 allfile.style();
 allfile.pr();
