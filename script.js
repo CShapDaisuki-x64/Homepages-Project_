4d57091d1e20db2ac60075c4164d6f2978f7b246
CShapDaisuki-x64 <235272469+CShapDaisuki-x64@users.noreply.github.com>
2026-09-11 20:33:51 +0900

    S4.2.0_D2.5.3_A1.0.0_M1.2.3_G1.2.4_U1.2.3_R2.0.2_V1.0.8_1 (ハンバーガーメニュー Fixes #6)

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ import * as allfile from './allfile.js';
 allfile.open_main();
 allfile.footer("/header.html","hedaer_div");
 allfile.footer("/footer.html","footer_div");
+allfile.footer("/menu.html","menu");
 allfile.style();
 allfile.pr();
