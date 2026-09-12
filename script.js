e469fefe6af8dfc4937079bc73f46482cd63faec
CShapDaisuki-x64 <235272469+CShapDaisuki-x64@users.noreply.github.com>
2026-09-12 14:16:23 +0900

    S4.1.7_D2.5.3_A1.0.0_M1.2.3_G1.2.4_U1.2.3_R2.0.2_V1.0.8_1 メニューを調整, 共有ボタンを作った

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ allfile.open_main();
 allfile.footer("/header.html","hedaer_div");
 allfile.footer("/footer.html","footer_div");
 allfile.footer("/nav.html","nav_div");
+allfile.footer("/share.html","nav_share_div");
 allfile.style();
 allfile.pr();
