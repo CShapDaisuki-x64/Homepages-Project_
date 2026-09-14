66bace34527e922f931cdb9b5c4ab4bd7cbbfbac
CShapDaisuki-x64 <235272469+CShapDaisuki-x64@users.noreply.github.com>
2026-09-14 19:56:32 +0900

    S4.2.2_D2.5.3_A1.0.0_M1.2.3_G1.2.4_U1.2.3_R2.0.2_V1.0.8_1

---
+++ seartch.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+// 同期的にファイルをUTF-8文字列として読み込む
+
+const targetDir = './'; // 取得したいディレクトリのパス
+let fileslo=[];
+try {
+  // recursive: true でサブディレクトリ内のファイルやフォルダも全て取得
+  const files = fs.readdirSync(targetDir, { recursive: true, encoding: 'utf8' });
+
+  // フルパスに変換したい場合やファイルだけを絞り込む場合の処理
+  files.forEach(file => {
+		if(file.endsWith('.html'))
+		{
+    	fileslo.push(file);
+		}
+  });
+} catch (err) {
+  console.error('エラーが発生しました:', err);
+}
+for(let i=0;i<fileslo.length;i++)
+{
+	const data = fs.readFileSync(fileslo[i], 'utf-8');
+	const name_0=/\s*<meta\s*name\s*=\s*["']robots["']\s*content\s*=\s*["'][^"']*(noindex|nofollow)[^"']*["']\s*>/i;
+	// 特定の文字列が含まれているか判定,複数を防止
+	if (name_0.test(data)&&!data.includes("data-pagefind-ignore"))
+	{
+		fs.writeFileSync(fileslo[i], data.replace("<html", '<html data-pagefind-ignore'), 'utf-8');
+	}
+	else
+	{
+		console.log('文字列は見つかりませんでした');
+	}
+}
